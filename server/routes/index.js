dc84d80eafb92df7196506a53836d0b9a7f6bc86
kru1980 <kru71101@gmail.com>
2019-02-19 20:26:49 +0500

    add responce todos

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const router = express.Router();
+const todos = require("../api/todos.json");
 
-router.get("/movies", (req, res) => {
-  res.end("We made it! And it's great");
+router.get("/todos", (req, res) => {
+  res.send(todos);
 });
 
 module.exports = router;
